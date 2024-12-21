2d3b0ffbfac5753f6f3c63a4c1335265bfa2a50f
faheemahmedmalik <faheem.malik@hotmail.com>
2024-12-21 14:40:20 +0000

    get functions added

--- CPS5001_Assessment/src/Address.java
+++ CPS5001_Assessment/src/Address.java
@@ -6,11 +6,37 @@ public class Address {
     private String country;
 
 
-
     public Address(String street, String city, String postalCode, String country) {
         this.street = street;
         this.city = city;
         this.postalCode = postalCode;
         this.country = country;
     }
+
+
+    public String get_Street_name() {
+        return street;
+    }
+
+    public String get_City_name() {
+        return city;
+    }
+
+    public String get_PostCode() {
+        return postalCode;
+    }
+
+    public String get_Country_name() {
+        return country;
+    }
+
+
+    public String display_Address() {
+        return street + ", " + city + ", " + postalCode + ", " + country;
+    }
+
+
+
+
+
 }
