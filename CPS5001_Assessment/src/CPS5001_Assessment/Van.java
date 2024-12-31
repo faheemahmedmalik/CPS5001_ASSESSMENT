752340eeb43340358b6cf0b52ff0e9544b7cddd3
faheemahmedmalik <faheem.malik@hotmail.com>
2024-12-31 23:53:01 +0500

    van attributes

--- CPS5001_Assessment/src/CPS5001_Assessment/Van.java
+++ CPS5001_Assessment/src/CPS5001_Assessment/Van.java
@@ -1,4 +1,20 @@
 package CPS5001_Assessment;
+ import java.util.ArrayList;
+ import java.util.List;
+
 
 public class Van {
+
+    private String id;
+    private double capacity;
+    private double currentLoad;
+    private List<Parcel> parcels;
+
+
+
+
+
+
+
+
 }
