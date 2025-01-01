538c22656b3ff78f96988ba17e178c3946c5169e
faheemahmedmalik <faheem.malik@hotmail.com>
2025-01-02 04:53:00 +0500

    creted Address object named Customer address1

--- CPS5001_Assessment/src/CPS5001_Assessment/Main.java
+++ CPS5001_Assessment/src/CPS5001_Assessment/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         Address hubAddress = new Address("123 Main Street", "London", "E1 1Ea", "UK");
 
+        Address customerAddress1 = new Address("456 Elm Street", "London", "SW1 1Ra", "UK");
 
 
 
