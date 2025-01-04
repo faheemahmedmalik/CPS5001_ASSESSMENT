f39977db628229cdf096a9f88f2423aa1f970b3e
faheemahmedmalik <faheem.malik@hotmail.com>
2025-01-05 04:14:05 +0500

    structure for DeliveryScheduler class

--- CPS5001_Assessment/src/CPS5001_Assessment/DeliveryScheduler.java
+++ CPS5001_Assessment/src/CPS5001_Assessment/DeliveryScheduler.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 
 public class DeliveryScheduler {
+    Graph g;
+    Map<Van, List<Parcel>> v = new HashMap<>();
+    PriorityQueue<Parcel> p = new PriorityQueue<>(Comparator.comparingDouble(Parcel::getDeadline));
+
+    public DeliveryScheduler(Graph graph) {
+        this.g = graph;
+    }
+
+
+
+
+
 }
