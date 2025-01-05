2401e82bd62b995ee56e6021bc7165f9228171b7
faheemahmedmalik <faheem.malik@hotmail.com>
2025-01-05 22:53:50 +0500

    initialise graph  and scheduler class

---
+++ CPS5001_Assessment/src/CPS5001_Assessment/CityLogisticsTerminal.java
@@ -0,0 +1,25 @@
+package CPS5001_Assessment;
+
+import java.util.Scanner;
+
+public class CityLogisticsTerminal {
+    private Graph graph = new Graph();
+    private DeliveryScheduler scheduler = new DeliveryScheduler(graph);
+
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public DeliveryScheduler getScheduler() {
+        return scheduler;
+    }
+
+
+
+
+
+
+
+
+}
+
