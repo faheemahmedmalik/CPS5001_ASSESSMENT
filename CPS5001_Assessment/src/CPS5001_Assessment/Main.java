4607f719cb4038af555d569c647f828572259f34
faheemahmedmalik <faheem.malik@hotmail.com>
2025-01-05 23:23:02 +0500

    create address object for hub and customers and node object

--- CPS5001_Assessment/src/CPS5001_Assessment/Main.java
+++ CPS5001_Assessment/src/CPS5001_Assessment/Main.java
@@ -7,14 +7,20 @@ public class Main {
         CityLogisticsTerminal terminal = new CityLogisticsTerminal();
 
 
-
         Address hubAddress = new Address("123 Main Street", "London", "E1 1Ea", "UK");
-
         Address customerAddress1 = new Address("456 Elm Street", "London", "SW1 1Ra", "UK");
+        Address customerAddress2 = new Address("789 Oak Avenue", "London", "SW12 5BA", "UK");
+        Address customerAddress3 = new Address("692 Waldegrave Rd","London", "TW1 4SX"  , "UK");
+        Address customerAddress4 = new Address("12 Milverton Rd", "London", "NW1 1AA", "UK");
 
 
-
+        // Predefined Node objects
         Node hubA = new Node("N1", "Hub A", hubAddress);
+        Node customerB = new Node("N2", "Customer B", customerAddress1);
+        Node customerC = new Node("N3", "Customer C", customerAddress2);
+        Node StmarysUniversity = new Node("N4", "StmarysUniversity", customerAddress3);
+        Node customerE = new Node("N5", "Customer E", customerAddress4);
+
 
 
 
