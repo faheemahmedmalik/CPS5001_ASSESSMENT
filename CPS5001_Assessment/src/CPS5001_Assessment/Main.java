34cf26b9495579270466cb250ccf2144e9807b8c
faheemahmedmalik <faheem.malik@hotmail.com>
2025-01-05 23:20:12 +0500

    initialise citylogisticesterminal and create terminal object

--- CPS5001_Assessment/src/CPS5001_Assessment/Main.java
+++ CPS5001_Assessment/src/CPS5001_Assessment/Main.java
@@ -4,6 +4,10 @@ package CPS5001_Assessment;
 public class Main {
     public static void main(String[] args) {
 
+        CityLogisticsTerminal terminal = new CityLogisticsTerminal();
+
+
+
         Address hubAddress = new Address("123 Main Street", "London", "E1 1Ea", "UK");
 
         Address customerAddress1 = new Address("456 Elm Street", "London", "SW1 1Ra", "UK");
